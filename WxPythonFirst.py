f0ea3a8ca040a02b3d64eb218a410b51bdd69f11
sh4d0w <digit4lsh4d0w@ya.ru>
2020-04-04 03:07:16 +0900

    Remove GUI

--- WxPythonFirst.py
+++
@@ -1,5 +0,0 @@
-import tkinter
-
-root = tkinter.Tk()
-root.filename = tkinter.filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"), ("all files","*.* ")))
-print(root.filename)

c14e12d27c9ea65430032532b8846db4ec532701
sh4d0w <digit4lsh4d0w@ya.ru>
2020-04-04 23:30:29 +0900

    Export passwords from all txt or csv TO ONE FILE

---
+++ AllInOne.py
@@ -0,0 +1,23 @@
+import os, shutil
+
+EXTENSION = ['txt', 'csv']
+FilesForReading = []
+listFiles = os.listdir()
+
+for file in listFiles:
+	if file.split('.')[-1] in EXTENSION:
+		FilesForReading.append(file)
+
+if FilesForReading:
+	allPass = open('AllPasswords.txt', 'w', encoding='utf-8')
+	for file in FilesForReading:
+		print('#%s File: %s'%(FilesForReading.index(file), file))
+		listOfPasswords = open('%s'%(file), 'r', encoding='utf-8')
+		try:
+			allPass.write(listOfPasswords.read() + '\n')
+		except UnicodeDecodeError:
+			print('WRONG SYMBOL')
+		listOfPasswords.close()
+	allPass.close()
+else:
+	print('No Files Left')
